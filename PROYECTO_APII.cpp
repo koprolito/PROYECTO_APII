50d32d5267e69c42f693dd692ea58e56b8f665d8
koprolito <130481373+koprolito@users.noreply.github.com>
2023-05-23 09:51:09 -0400

    Modificaciones 23/5/23 - 9:51am

--- PROYECTO_APII.cpp
+++ PROYECTO_APII.cpp
@@ -1,87 +1,88 @@
- #include <iostream>  
- #include <windows.h>  
- #include <stdlib.h> 
- #include <string>
- #include <time.h>
- using namespace std;
+#include <iostream>  
+#include <windows.h>  
+#include <stdlib.h> 
+#include <string>
+#include <time.h>
+#include <cstdlib>
+using namespace std;
 
 struct nodo{
-int car;
-int i=0;
-int wini=0;
-int r;
-int rpos, rpos1, rpos12;
-string vel="";
-int cvel;
-int pos;
-struct nodo* prox;
+    int car;
+    int i=0;
+    int wini=0;
+    int r;
+    int rpos, rpos1, rpos12;
+    string vel="";
+    int cvel;
+    int pos;
+    struct nodo* prox;
 };
 
 typedef struct nodo* TLista;
 
 
 TLista crearNodo(int Tcar, string Tvel, int Tcvel, int Ttpos){
-TLista q;
-q= new(struct nodo);
-q->car=Tcar;
-q->vel=Tvel;
-q->cvel=Tcvel;
-q->pos=Ttpos;
-return q;
-}
+    TLista q;
+    q= new(struct nodo);
+    q->car=Tcar;
+    q->vel=Tvel;
+    q->cvel=Tcvel;
+    q->pos=Ttpos;
+    return q;
+}   
 
-void insertarFinal(TLista &lista,int Tcar1, string Tvel1, int Tcvel1, int Ttpos1){
-  TLista q= crearNodo(Tcar1, Tvel1,Tcvel1, Ttpos1);
-  if (lista==NULL){
-    lista=q;
-  }
-  else{
-  /*TLista aux= lista;
-    while (aux->prox != NULL){
-        aux= aux->prox;
+TLista insertarFinal(TLista &lista,int Tcar1, string Tvel1, int Tcvel1, int Ttpos1){
+    TLista p = crearNodo(Tcar1, Tvel1,Tcvel1, Ttpos1), aux = lista, aux1 = aux;
+    if(aux == NULL){
+        p->prox = aux;
+        return p;
     }
-    aux->prox=q; */
-    q->prox=lista;
-    lista=q;
-  }
+    else{
+        while(aux->prox != NULL){
+            aux = aux->prox;
+        }
+        p->prox = aux->prox;
+        aux->prox = p;
+        return aux1;
+    }
 }
 
 
 
- void gotoxy(int x,int y){  
-      HANDLE hcon;  
-      hcon = GetStdHandle(STD_OUTPUT_HANDLE);  
-      COORD dwPos;  
-      dwPos.X = x;  
-      dwPos.Y= y;  
-      SetConsoleCursorPosition(hcon,dwPos);  
- }  
+void gotoxy(int x,int y){  
+    HANDLE hcon;  
+    hcon = GetStdHandle(STD_OUTPUT_HANDLE);  
+    COORD dwPos;  
+    dwPos.X = x;  
+    dwPos.Y= y;  
+    SetConsoleCursorPosition(hcon,dwPos);  
+}  
 
 
 void moverCarro(TLista &lista1){
-     lista1->i=lista1->i+1;
-      if (lista1->i<80){ 
-      gotoxy(lista1->i,lista1->pos);  
-      cout<<lista1->vel<<"*";
-      }
+    lista1->i=lista1->i+1;
+    if (lista1->i<80){ 
+        gotoxy(lista1->i,lista1->pos);  
+        cout<<lista1->vel<<"*";
+        }
 }
 
 void limpiar(TLista &lista1){
     if (lista1->i<=80){ 
-      gotoxy(lista1->i,lista1->pos);  
-      cout<<lista1->vel<<" ";
-      }
+        gotoxy(lista1->i,lista1->pos);  
+        cout<<lista1->vel<<" ";
+    }
 }
 
 
 void ganador(TLista lista1, int *twin){
     if ((lista1->i>=80)){
-          if (lista1->wini==0){
-          gotoxy(80,lista1->pos);
-          *twin=*twin+1;
-          cout<<"| #"<<*twin<<" lugar";
-          lista1->wini=1;     
-      }
+        if (lista1->wini==0){
+            gotoxy(80,lista1->pos);
+            *twin=*twin+1;
+            cout<<"| #"<<*twin<<" lugar";
+            lista1->wini=1;     
+        }
     }
 
 }
@@ -117,89 +118,89 @@ void Carre(TLista &lista, int tcant, int tposUlt){
 	int m=0,s=0;
 
 
- //meta
-n1=lista;
-for (int v=1; v<=tcant;v++){
-    gotoxy(80,n1->pos); cout<<"|"; 
-    n1=n1->prox;
-}
+    //meta
+    n1=lista;
+    for (int v=1; v<=tcant;v++){
+        gotoxy(80,n1->pos); cout<<"|"; 
+        n1=n1->prox;
+    }
 
-n1=lista;
-    do{
+    n1=lista;
+        do{
+            for (int v=1;v<=tcant;v++){
+                moverCarro(n1);
+                n1=n1->prox;
+            }
+            n1=lista;
+
+
+        //temp
+                
         for (int v=1;v<=tcant;v++){
-            moverCarro(n1);
+            ult+=2;
+                if (n1->i<=80){
+                    gotoxy(2,ult);
+                    cout<<"Carrito "<<v<<": "<<endl;
+                    cout << m << ":" << s << endl;
+                }
+
+        }
+                ult=tposUlt;
+
+
+                
+
+    s++;
+    if (s==60){s=0;m=m+1;}
+
+        Sleep(500); //intentar sleep en 600
+
+
+        //limpiar el * de la pantalla
+        for (int v=1; v<=tcant;v++){
+            limpiar(n1); 
             n1=n1->prox;
+            //puede q sea eso y si n1->prox = null, hacer break
         }
         n1=lista;
 
+        
 
-       //temp
-            
-    for (int v=1;v<=tcant;v++){
-        ult+=2;
-            if (n1->i<=80){
-                gotoxy(2,ult);
-                cout<<"Carrito "<<v<<": "<<endl;
-		        cout << m << ":" << s << endl;
-            }
+        //obstaculo
+        for (int v=1; v<=tcant;v++){
+        if (n1->r==1){
+            if ((n1->i<n1->rpos1)||(n1->i>n1->rpos12)){
+            n1->i=n1->i+n1->cvel;}
+        }
+        else{n1->i=n1->i+n1->cvel;}
+            n1=n1->prox;
+        }
+        n1=lista;
+        
 
-    }
-            ult=tposUlt;
-
-
-			
-
-s++;
-if (s==60){s=0;m=m+1;}
-
-      Sleep(500); //intentar sleep en 600
-
-
-      //limpiar el * de la pantalla
-    for (int v=1; v<=tcant;v++){
-        limpiar(n1); 
-        n1=n1->prox;
-        //puede q sea eso y si n1->prox = null, hacer break
-    }
-    n1=lista;
-
-    
-
-      //obstaculo
-    for (int v=1; v<=tcant;v++){
-    if (n1->r==1){
-        if ((n1->i<n1->rpos1)||(n1->i>n1->rpos12)){
-        n1->i=n1->i+n1->cvel;}
-    }
-    else{n1->i=n1->i+n1->cvel;}
-        n1=n1->prox;
-    }
-    n1=lista;
-    
-
-      //limpiar cronometro
-    for (int v=1; v<=tcant;v++){
-        ult+=2;
-        if (n1->i<80){gotoxy(2,ult); cout<<"          ";}
-        n1=n1->prox;
-    }
-    n1=lista;
-    ult=tposUlt;
+        //limpiar cronometro
+        for (int v=1; v<=tcant;v++){
+            ult+=2;
+            if (n1->i<80){gotoxy(2,ult); cout<<"          ";}
+            n1=n1->prox;
+        }
+        n1=lista;
+        ult=tposUlt;
 
 
-      //verificar ganador
-    for (int v=1; v<=tcant;v++){
-        ganador(n1,&win);
-        n1=n1->prox;
-    }
-    n1=lista;
-    
+        //verificar ganador
+        for (int v=1; v<=tcant;v++){
+            ganador(n1,&win);
+            n1=n1->prox;
+        }
+        n1=lista;
+        
 
 
 
 
-      //cuando todos terminen, terminar el ciclo          
-        }while(win<tcant);
+    //cuando todos terminen, terminar el ciclo          
+    }while(win<tcant);
 
 }
 
@@ -211,49 +212,49 @@ int main(){
     string Bvel, res;
     int cant, h, g, Bcar;
     int posUlt;
-
     do{
-        system("cls");
-        cout<<"Indique la cantidad de carritos. Minimo 2 y maximo 12"<<endl;
-        cin>>cant;
-        h=2;
-        TLista p=NULL;
-        for (int b=1; b<=cant; b++){
-            g=0;
-            cout<<"Indique velocidad del carrito "<<b<<": ";
-            cin>>(Bcvel);
-            Bcar=b;
-            do{
-                g++;
-                Bvel=Bvel+" ";              
-            }while (g!=Bcvel);
-            Bpos=h;
-            h=h+2;
-            if (b==cant){
-                posUlt=Bpos;
-            }
+            system("cls");
+            cout<<"Indique la cantidad de carritos. Minimo 2 y maximo 12:\n\n";
+            cin>>cant;
+            h=2;
+            TLista p=NULL;
+            for (int b=1; b<=cant; b++){
+                g=0;
+                cout<<"\n\nIndique velocidad del carrito "<<b<<": ";
+                cin>>(Bcvel);
+                Bcar=b;
+                do{
+                    g++;
+                    Bvel=Bvel+" ";              
+                }while (g!=Bcvel);
+                Bpos=h;
+                h=h+2;
+                if (b==cant){
+                    posUlt=Bpos;
+                }
 
-            insertarFinal(p,Bcar, Bvel,Bcvel, Bpos);
+                p = insertarFinal(p,Bcar, Bvel,Bcvel, Bpos);
+                
+            }
+            TLista n=p;
+            for (int f=1;f<=cant;f++){
+                gotoxy(1,n->pos);cout<<"*"; gotoxy(80,n->pos);cout<<"|";
+                n=n->prox;
+            }
+            n=p;
+            system("cls");
+            cout << "\n\nEl proceso de lectura de los datos de los carros ha finalizado exitosamente" << endl;
+            cout << "\nPulse cualquier tecla para iniciar la carrera"<<endl;
+            system("pause");
+            system("cls");
             
-        }
-        TLista n=p;
-        for (int f=1;f<=cant;f++){
-            gotoxy(1,n->pos);cout<<"*"; gotoxy(80,n->pos);cout<<"|";
-            n=n->prox;
-        }
-        n=p;
-        system("cls");
-        cout<<"pulse cualquier tecla para iniciar la carrera"<<endl;
-        system("pause");
-        system("cls");
-        
-        Carre(n,cant,posUlt);
-        
-        cout<<endl;
-        cout<<endl;
-        cout<<"iniciar de nuevo? 1 Si y 2 No"<<endl;
-        cin>>res2;
-}while(res2==1);
+            Carre(n,cant,posUlt);
+            
+            cout<<endl;
+            cout<<endl;
+            cout<<"iniciar de nuevo? 1 Si y 2 No"<<endl;
+            cin>>res2;
+    }while(res2==1);
 
     return 0;  
 }
